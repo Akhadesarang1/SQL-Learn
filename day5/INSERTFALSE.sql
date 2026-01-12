5b2d74e2133848818ff630ed6a203ead3da9ad20
Sarang Akhade <Akhadesarang1@gmail.com>
2026-01-12 17:39:54 +0530

    descriptive example
    Intentionally added wrong value of yearly salary to demonstrate constraint violation.

--- day5/INSERTFALSE.sql
+++ day5/INSERTFALSE.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (full_name, yearly_salary, current_status)
 VALUES ('ddajbhf', -23344, 'Employed');
 
---Check constraint 'yearly_salary_positive' is violated.
+--Check constraint 'yearly_salary_positive' is violated.intentionally added wrong value of yearly salary 
