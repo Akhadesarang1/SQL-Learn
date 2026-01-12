294741c43524848a723e60cf3f84f1b4aace93b6
Akhadesarang1 <akhadesarang1@gmail.com>
2026-01-12 14:21:42 +0530

    Remove Day4 constraints SQL file

--- Day4/Constraints.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE users
- --postgres
- ALTER COLUMN full_name SET NOT NULL,
- ALTER COLUMN current_status SET NOT NULL;
--- -- --mysql
--- MODIFY COLUMN full_name VARCHAR(300) SET NOT NULL;
