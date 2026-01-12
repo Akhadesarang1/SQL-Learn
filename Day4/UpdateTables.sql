17fb371cc241127a0f50f980602d10fa8955d05d
Akhadesarang1 <akhadesarang1@gmail.com>
2026-01-12 11:31:17 +0530

    Updating table and columns

--- Day4/UpdateTables.sql
+++ Day4/UpdateTables.sql
@@ -1,2 +1,5 @@
-UPDATE table_name
-SET column_name = ;
+-- ALTER TABLE employers
+-- ALTER COLUMN yearly_revenue SET DATA TYPE FLOAT; -- postgresql
+
+ALTER TABLE employers
+MODIFY COLUMN yearly_revenue FLOAT(5, 2); -- MYSQL
