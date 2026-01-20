c72d377897545e6409bd0e1332f8a5e4aba6d2ce
Akhadesarang1 <akhadesarang1@gmail.com>
2026-01-20 16:57:06 +0530

    Written queries for relations

---
+++ Day10/Prisms/Insertcompanydata.sql
@@ -0,0 +1,5 @@
+INSERT INTO employees (first_name, last_name, birth_date, email)
+VALUES ('Sarang', 'Akhade', '2002-06-27', 'sarang@test.com');
+
+INSERT INTO intranet_accounts (email, pass_word)
+VALUES ('sarang@test.com', 'abcae1');
